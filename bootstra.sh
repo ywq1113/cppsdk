3db5fbdcfcbd9f9b53d60fff693c4e34c0656304
wizyang <419083353@qq.com>
2025-07-31 16:20:54 +0800

    update

--- bootstra.sh
+++ bootstra.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo apt-get -y install bazel-bootstrap \
+sudo apt-get -y install curl \
     build-essential
+
+curl -L https://github.com/bazelbuild/bazel/releases/download/8.3.1/bazel-8.3.1-installer-linux-x86_64.sh -o bazel-installer.sh
+chmod +x bazel-installer.sh
+sudo ./bazel-installer.sh
+sudo rm bazel-installer.sh
